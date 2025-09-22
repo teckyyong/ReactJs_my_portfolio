8dad392d2e272ed482bd748ecf915bfdbee1979f
tlai0018 <tlai008@student.monash.edu>
2025-09-22 16:08:11 +0800

    Initial commit - ReactJS portfolio project

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,6 +8,7 @@ const About= () => {
     return(
         <div id='about' className='about'>
           <div className="about-title">
+            
             <h1>About me</h1>
             <img src={theme_pattern} alt="" />
           </div>
